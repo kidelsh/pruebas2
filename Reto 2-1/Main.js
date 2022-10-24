da4b315220637cf9ffc73e0d0109c9d974d34d3a
kidelsh <115489955+kidelsh@users.noreply.github.com>
2022-10-24 18:11:06 -0300

    Reto 2-1

---
+++ Reto 2-1/Main.js
@@ -0,0 +1,16 @@
+var animal1 = "Pedrito"
+var animal2 = "Rufo"
+var animal3 = "Chancla"
+
+console.log ("mis mascotas son", animal1,",", animal2, "y", animal3, ". Les quiero mucho")
+
+var mascota1 = prompt("Escribe el nombre de tu primera mascota")
+var mascota2 = prompt("Escribe el nombre de tu segunda mascota")
+
+console.log ("Quiero mucho a mis mascotas.", mascota1, "y", mascota2)
+
+var numero = "100"
+console.log (numero)
+console.log (typeof numero)
+var numero = 100
+console.log (typeof numero)
