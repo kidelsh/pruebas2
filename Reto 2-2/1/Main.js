f782660a0e578b73c232fb97deae34358e2affac
kidelsh <115489955+kidelsh@users.noreply.github.com>
2022-10-24 18:12:42 -0300

    Reto 2

---
+++ Reto 2-2/1/Main.js
@@ -0,0 +1,5 @@
+let n1 = Number(prompt ("Primera Nota"));
+let n2 = Number(prompt ("segunda nota"));
+let n3 = Number(prompt ("tercera nota"));
+
+alert ((n1 + n2 + n3) / 3)
---
+++ Reto 2-2/2/Main.js
@@ -0,0 +1,8 @@
+var edad = prompt("Introduce tu edad")
+var estatura = prompt("Introduce tu estatura en CENTIMETROS")
+
+if( edad >= 18 && estatura >= 160 ) 
+    alert ("Acceso permitido")
+else (
+    alert ("Acceso denegado")
+)
